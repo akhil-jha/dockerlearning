9528611aa203855eb0b08320c9d05a433844a8c0
Akhil Jha <akjha@redhat.com>
2021-05-20 23:17:53 +0530

    updating test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	  qweqfow
+	  qwertyuiop
 	</p>  
 	  <a
           className="App-link"
